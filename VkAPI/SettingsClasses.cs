071050b5bb6e6eb244dae8aa5b9f9d677b3a6a75
Itua-Juele <ituazhuel@hotmail.com>
2017-02-02 22:59:04 +0300

    0.0.3 VK класс переделан под static class. Задана структура по которой будут строится дальнейшие классы и методы

---
+++ VkAPI/SettingsClasses.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace VkAPI
+{
+    /// <summary>
+    /// Класс содержащий информацию о пользователе
+    /// </summary>
+    public class VkUser
+    {
+        /// <summary>
+        /// id пользователя
+        /// </summary>
+        public string id;
+        /// <summary>
+        /// Уникальный ключ пользователя
+        /// </summary>
+        public string token;
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string FirstName;
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string LastName;
+        /// <summary>
+        /// Полная информация о пользователе
+        /// </summary>
+        public Dictionary<string, string> Info;
+        /// <summary>
+        /// Содержит последний ответ об ошибке сервера
+        /// </summary>
+        public Dictionary<string, string> Error;
+
+        /// <summary>
+        /// Конструктор класса VkUser, в котором сразу выясняются параметры FirstName и LastName
+        /// </summary>
+        /// <param name="id">id пользователя</param>
+        public VkUser(string id)
+        {
+            this.id = id;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            try
+            {
+                Dictionary<string, string> data = VK.Users.Get(new string[] { id })[0];
+                if (!data.ContainsKey("error_code"))
+                {
+                    FirstName = data["first_name"];
+                    LastName = data["last_name"];
+                    Error = new Dictionary<string, string>();
+                    Info = new Dictionary<string, string>(data);
+                }
+                else
+                {
+                    Error = new Dictionary<string, string>(data);
+                    Info = new Dictionary<string, string>();
+                }
+                data.Clear();
+            }
+            catch
+            {
+                Info = new Dictionary<string, string>();
+                Error = new Dictionary<string, string>();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Класс для обработки ответов от сервера VK
+    /// </summary>
+    static class VkJson
+    {
+        /// <summary>
+        /// Возвращает список из словарей, содержащихся в строке
+        /// </summary>
+        /// <param name="dataJson">строка вида [словарь, словарь..]</param>
+        /// <returns></returns>
+        public static string[] ListDictionary(string dataJson)
+        {
+            int basket = 0;
+            string str = "";
+            char s;
+            for (int i = 1; i < dataJson.Length - 1; i++)
+            {
+                s = dataJson[i];
+                if (s == '{')
+                {
+                    basket++;
+                }
+                else if (s == '}')
+                {
+                    basket--;
+                }
+
+                if ((s == ',') & (basket == 0))
+                {
+                    str += "\n";
+                }
+                else
+                {
+                    str += s;
+                }
+            }
+            return str.Split('\n');
+        }
+
+        /// <summary>
+        /// Преобразует ответ сервера в словарь, если ответом сервера была ошибка
+        /// </summary>
+        /// <param name="errorJson">ответ сервера</param>
+        /// <returns></returns>
+        public static Dictionary<string, string> ResponseError(string errorJson)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            FillDictionary(ref data, errorJson.Substring(9, errorJson.Length - 10), "");
+            return data;
+        }
+
+        /// <summary>
+        /// Заполняет словарь в соответствии с ответом сервера
+        /// </summary>
+        /// <param name="data">словар для заполнения</param>
+        /// <param name="json">ответ сервера в json формате, без внешнего словаря response или error</param>
+        /// <param name="key">значение, которое добавляется перед ключем</param>
+        public static void FillDictionary(ref Dictionary<string, string> data, string json, string key)
+        {
+            string[] jsonLines = SerializeJson(json).Split('\n');
+            if (data == null)
+            {
+                data = new Dictionary<string, string>();
+            }
+            // Удаляет в строке ненужные символы
+            string[] jsonLine;
+            char s;
+            for (int i = 0; i < jsonLines.Length; i++)
+            {
+                if (i < jsonLines.Length - 1)
+                // Удаляем запятые
+                {
+                    jsonLines[i] = jsonLines[i].Substring(0, jsonLines[i].Length - 1);
+                }
+
+                jsonLine = jsonLines[i].Split(new char[] { ':' }, 2);
+                s = jsonLine[1][0];
+                if (s == '"')
+                {
+                    if (jsonLine[1].Length == 2)
+                    {
+                        jsonLine[1] = "information is absent";
+                    }
+                    // Удаляем кавычки в значении valeu
+                    jsonLines[i] = jsonLine[0] + ":" + jsonLine[1].Substring(1, jsonLine[1].Length - 2);
+                }
+            }
+
+            // Заполняем словарь
+            string[] keyValeu;
+            foreach (string line in jsonLines)
+            {
+                keyValeu = line.Split(new char[] { ':' }, 2);
+
+                if (keyValeu[1].Substring(0, 1) == "{")
+                // Если в значении value находится словарь
+                {
+                    FillDictionary(ref data, keyValeu[1], key + "*");
+                }
+                else
+                {
+                    data.Add(key + keyValeu[0].Substring(1, keyValeu[0].Length - 2), keyValeu[1]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Преобразует строку Json в читабельный вид
+        /// </summary>
+        /// <param name="str">строка json-формата</param>
+        /// <returns></returns>
+        private static string SerializeJson(string str)
+        // Преобразует строку в Json формат <string, string>
+        {
+            int basket = -1;
+            int quotes = 0;
+            string json = "";
+            char s;
+            for (int i = 0; i < str.Length; i++)
+            {
+                json += str[i];
+
+                // Отслеживаем уровень, чтобы не попасть во внутрений словарь
+                s = str[i];
+                if ((s == '{') | (s == '['))
+                {
+                    basket++;
+                }
+                else if ((s == '}') | (s == ']'))
+                {
+                    basket--;
+                }
+
+                // Отслеживаем закрытость ковычек
+                if ((quotes == 0) & (s == '"'))
+                {
+                    quotes++;
+                }
+                else if ((quotes == 1) & (s == '"'))
+                {
+                    quotes--;
+                }
+
+                if (((s == ',') & (basket == 0) & (quotes == 0)) | (i == str.Length))
+                {
+                    json += "\n";
+                }
+            }
+            return json.Substring(1, json.Length - 2);
+        }
+
+        /// <summary>
+        /// Возвращает ответ сервера в json виде
+        /// </summary>
+        /// <param name="url">адрес по которму происходит запрос</param>
+        /// <returns></returns>
+        public static string getResponse(string url)
+        {
+            WebRequest reqGET = WebRequest.Create(url);
+            WebResponse resp = reqGET.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string s = sr.ReadToEnd();
+            sr.Dispose();
+            return s;
+        }
+    }
+}
---
+++ VkAPI/UsersClass.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+namespace VkAPI
+{
+    /// <summary>
+    /// Клас для работы с VK API
+    /// </summary>
+    public static partial class VK
+    {
+        /// <summary>
+        /// Класс для работы с Users методами
+        /// </summary>
+        public static class Users
+        {
+            // Метод user.get |------------------------------------------------
+            private static Dictionary<string, string>[] VkGet(string url)
+            {
+                string dataJson = VkJson.getResponse(url);
+                Dictionary<string, string>[] data = new Dictionary<string, string>[1];
+                if (dataJson.Substring(2, 5) == "error")
+                {
+                    data = new Dictionary<string, string>[1] { VkJson.ResponseError(dataJson) };
+                }
+                else
+                {
+                    string[] response = VkJson.ListDictionary(dataJson.Substring(12, dataJson.Length - 13));
+                    data = new Dictionary<string, string>[response.Length];
+                    for (int i = 0; i < response.Length; i++)
+                    {
+                        VkJson.FillDictionary(ref data[i], response[i], "");
+                    }
+                }
+                return data;
+            }
+            /// <summary>
+            /// Возвращает общую информацию о пользователях
+            /// </summary>
+            /// <param name="ids">id пользователей</param>
+            /// <returns></returns>
+            public static Dictionary<string, string>[] Get(string[] ids)
+            {
+                string url = String.Format("https://api.vk.com/method/users.get?user_ids={0}",
+                    String.Join(",", ids));
+                return VkGet(url);
+            }
+            /// <summary>
+            /// Возвращает расширеную информацию о пользователях
+            /// </summary>
+            /// <param name="ids">id пользователей</param>
+            /// <param name="param">параметры, которые вы хотите получить</param>
+            /// <returns></returns>
+            public static Dictionary<string, string>[] Get(string[] ids, string[] param)
+            {
+                string url = String.Format("https://api.vk.com/method/users.get?user_ids={0}&fields={1}",
+                    String.Join(",", ids), String.Join(",", param));
+                return VkGet(url);
+            }
+            /// <summary>
+            /// Заносит в класс VkUser общую информацию о пользователе
+            /// </summary>
+            /// <param name="user"></param>
+            public static void Get(ref VkUser user)
+            {
+                string url = "https://api.vk.com/method/users.get?user_ids=" + user.id;
+                Dictionary<string, string> data = VkGet(url)[0];
+                if (data.ContainsKey("error_code"))
+                {
+                    user.Error = new Dictionary<string, string>(data);
+                }
+                else
+                {
+                    user.Info["first_name"] = data["first_name"];
+                    user.Info["last_name"] = data["last_name"];
+                    user.LastName = user.Info["last_name"];
+                    user.FirstName = user.Info["first_name"];
+                }
+                data.Clear();
+            }
+            /// <summary>
+            /// Заносит в класс VkUser информацию, указанную параметрами
+            /// </summary>
+            /// <param name="user"></param>
+            /// <param name="param">параметры запроса</param>
+            public static void Get(ref VkUser user, string[] param)
+            {
+                string url = String.Format("https://api.vk.com/method/users.get?user_ids={0}&fields={1}",
+                    user.id, String.Join(",", param));
+                Dictionary<string, string> data = VkGet(url)[0];
+                if (data.ContainsKey("error_code"))
+                {
+                    user.Error = new Dictionary<string, string>(data);
+                }
+                else
+                {
+                    foreach (string key in data.Keys)
+                    {
+                        if (user.Info.ContainsKey(key))
+                        {
+                            user.Info[key] = data[key];
+                        }
+                        else
+                        {
+                            user.Info.Add(key, data[key]);
+                        }
+
+                        if (key == "first_name")
+                        {
+                            user.FirstName = data[key];
+                        }
+                        else if (key == "last_name")
+                        {
+                            user.LastName = data[key];
+                        }
+                    }
+                }
+                data.Clear();
+            }
+        }
+    }
+}
+
